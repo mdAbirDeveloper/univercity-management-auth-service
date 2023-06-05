ecba103953080a32e8374e70fac9fc124f0b3f12
Sah-jalal <mdsahjalal9778@gmail.com>
2023-06-05 10:17:21 +0600

    lint is working

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,8 @@
+// import { Schema, model} from 'mongoose';
+
+// // 2. Create a Schema corresponding to the document interface.
+// const userSchema = new Schema<IUser>({
+//   id: {
+
+//   }
+// });
