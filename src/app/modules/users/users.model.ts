3ce4c99c61a79fe7e47670814965de98b3254610
Sah-jalal <mdsahjalal9778@gmail.com>
2023-06-06 21:33:10 +0600

    last commit

--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,8 +1,27 @@
-// import { Schema, model} from 'mongoose';
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users.interface'
+type UserModel = Model<IUser, object>
 
-// // 2. Create a Schema corresponding to the document interface.
-// const userSchema = new Schema<IUser>({
-//   id: {
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-//   }
-// });
+export const User = model<IUser, UserModel>('User', userSchema)
