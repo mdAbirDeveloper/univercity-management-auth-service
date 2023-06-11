ee619539ba6637ed4989902e6e339a777c4cd3c5
sah-jalal <abir.developer@gmail.com>
2023-06-11 20:27:57 +0600

    just check

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
-import express, { Application, Request, Response } from 'express'
+import express, {
+  Application,
+  NextFunction,
+  Request,
+  Response,
+  response,
+} from 'express'
 import cors from 'cors'
 
 const app: Application = express()
@@ -9,8 +15,32 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('working successfully')
+class ApiError extends Error {
+  statusCode: number
+
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message)
+    this.statusCode = statusCode
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  throw new Error('there was an error')
+  // next('there was an error') //error
+})
+
+//global error handler
+app.use((err, req: request, res: response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({ error: err })
+  } else {
+    res.status(500).json({ error: 'something went wrong' })
+  }
 })
 
 export { app, port }
