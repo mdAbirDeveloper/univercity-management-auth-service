1f064f346cd1c144e31d43abdab8344d551f9c3f
sah-jalal <abir.developer@gmail.com>
2023-06-11 17:03:53 +0600

    from laptop

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,33 +1,57 @@
 import { createLogger, format, transports } from 'winston'
 import path from 'path'
+import DailyRotateFile from 'winston-daily-rotate-file'
+
 const { combine, timestamp, label, printf } = format
 
 //Custom log format
 const myFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${date.toDateString()} ${hour}:${minute}:${seconds} [${label}] ${level}: ${message}`
 })
 
 const logger = createLogger({
   level: 'info',
-  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
   defaultMeta: { service: 'user-service' },
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'info',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu-%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
 const errorlogger = createLogger({
   level: 'error',
-  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
   defaultMeta: { service: 'user-service' },
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
-      level: 'error',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'phu-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
