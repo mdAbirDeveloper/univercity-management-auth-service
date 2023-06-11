ee1dfbccd6f71b77d6a0ee804a92dc244406c838
sah-jalal <abir.developer@gmail.com>
2023-06-11 10:09:30 +0600

    another-commit form laptop

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import { app, port } from './app'
 import config from './config/index'
+import { errorlogger, logger } from './shared/logger'
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('data base is connected successfully')
+    logger.info('data base is connected successfully')
 
     app.listen(port, () => {
-      console.log(`Application listening on port , this is checkd ${port}`)
+      logger.info(`Application listening on port ${port}`)
     })
   } catch (error) {
-    console.log('faild to connect database', error)
+    errorlogger.error('faild to connect database', error)
   }
 }
 
