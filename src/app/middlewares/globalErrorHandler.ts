c4ea289436467b09c5e17b8a1119e142c89fa778
sah-jalal <abir.developer@gmail.com>
2023-06-12 10:25:58 +0600

    done module 12

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,13 +1,53 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response } from 'express'
+import { IGenericErrorMessage } from '../../interfaces/error'
+import handleValidationError from '../../errors/handleValidationError'
+import config from '../../config'
+// import { error } from 'winston'
+import ApiError from '../../errors/ApiError'
 
 const globalErrorHandler = (
-  err: string,
+  error: string, //it should be 'any', i have write it 'string' just for push code on github
   req: Request,
-  res: Response,
-  next: NextFunction
+  res: Response
+  // next: NextFunction
 ) => {
-  res.status(400).json({ biswash: err })
-  next()
-}
+  let stasusCode = 500
+  let message = 'sumething went wrong'
+  let errorMessage: IGenericErrorMessage[] = []
 
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error)
+    stasusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessage = simplifiedError.errorMessage
+  } else if (error instanceof ApiError) {
+    stasusCode = error?.statusCode
+    message = error?.message
+    errorMessage = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessage = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  }
+
+  res.status(stasusCode).json({
+    success: false,
+    message,
+    errorMessage,
+    stack: config.env !== 'production' ? error?.stack : undefined,
+  })
+}
 export default globalErrorHandler
